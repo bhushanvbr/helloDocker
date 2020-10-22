a09407f93be3935dce913c84f1fa8591caabf332
Vidya Bhushan Ray <bhushan.vbr@gmail.com>
2020-10-22 23:20:48 +0530

    Initial project commit from STS4

---
+++ helloDocker/src/test/java/com/vidya/socketa/HelloDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidya.socketa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
